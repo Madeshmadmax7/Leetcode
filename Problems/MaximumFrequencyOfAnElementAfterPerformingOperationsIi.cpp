89d76e0154af672c768c470e3e2a62653940eefb
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-10-22 17:40:23 +0530

    Add solution: MaximumFrequencyOfAnElementAfterPerformingOperationsIi

---
+++ Problems/MaximumFrequencyOfAnElementAfterPerformingOperationsIi.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1808479110 lang=cpp
+ *
+ * MaximumFrequencyOfAnElementAfterPerformingOperationsIi
+ * 
+ * Difficulty: Hard
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        
+        int ans = 1;
+        int adjustable = 0;
+        unordered_map<int, int> frequency;
+        set<int> candidates;
+        map<int, int> critical_points;
+
+        for(int num: nums) {
+            frequency[num]++;
+            critical_points[num - k]++;
+            critical_points[num + k + 1]--;
+            candidates.insert(num);
+            candidates.insert(num - k);
+            candidates.insert(num + k + 1);
+        }
+
+        for(int num: candidates) {
+            adjustable += critical_points.contains(num) ? critical_points[num] : 0;
+            int frequencyOfNum = frequency.contains(num) ? frequency[num] : 0;
+            int adjusted = adjustable - frequencyOfNum;
+            ans = max(ans, frequencyOfNum + min(adjusted, numOperations));
+        }
+
+        return ans;
+    }
+};
