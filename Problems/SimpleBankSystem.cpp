293fc2157080169d6f886d23c687c489d93f2b03
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-10-26 13:48:11 +0530

    Add solution: SimpleBankSystem

---
+++ Problems/SimpleBankSystem.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=1812035979 lang=cpp
+ *
+ * SimpleBankSystem
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Bank {
+public:
+    vector<long long> balance;
+    int n;
+
+    Bank(vector<long long>& balance):balance(balance), n(balance.size()) {}
+
+    bool transfer(int account1, int account2, long long money) {
+        if (account1>n||account2>n||balance[account1-1]<money) 
+            return 0;
+        balance[account1-1]-=money;
+        balance[account2-1]+=money;
+        return 1;
+    }
+
+    bool deposit(int account, long long money) {
+        if (account>n) return 0;
+        balance[account-1]+=money;
+        return 1;
+    }
+
+    bool withdraw(int account, long long money) {
+        if (account>n||balance[account-1]<money) 
+            return 0;
+        balance[account-1]-=money;
+        return 1;
+    }
+};
