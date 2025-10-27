c7cb7f72580fba6504ad3557fda0d287077dfd07
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-10-27 18:20:20 +0530

    Add solution: NumberOfLaserBeamsInABank

---
+++ Problems/NumberOfLaserBeamsInABank.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=1813138476 lang=cpp
+ *
+ * NumberOfLaserBeamsInABank
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        if (bank.empty()) {
+            return 0;
+        }
+        
+        int totalBeams = 0;
+        int prevRowDevices = countDevices(bank[0]);
+        
+        for (int i = 1; i < bank.size(); i++) {
+            int currentRowDevices = countDevices(bank[i]);
+            
+            if (currentRowDevices == 0) {
+                continue;
+            }
+            
+            totalBeams += prevRowDevices * currentRowDevices;
+            prevRowDevices = currentRowDevices;
+        }
+        
+        return totalBeams;
+    }
+    
+private:
+    int countDevices(const string& row) {
+        int deviceCount = 0;
+        for (char cell : row) {
+            if (cell == '1') {
+                deviceCount++;
+            }
+        }
+        return deviceCount;
+    }
+};
