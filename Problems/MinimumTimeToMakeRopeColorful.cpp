6702c4140805a3c6124b3965c3b72f705c2917aa
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-03 18:17:24 +0530

    Add solution: MinimumTimeToMakeRopeColorful

---
+++ Problems/MinimumTimeToMakeRopeColorful.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1819537621 lang=cpp
+ *
+ * MinimumTimeToMakeRopeColorful
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        int totalTime = 0, currMaxTime = 0;
+        for (int i = 0; i < colors.size(); ++i) {
+            if (i > 0 && colors[i] != colors[i - 1]) {
+                currMaxTime = 0;
+            }
+            totalTime += min(currMaxTime, neededTime[i]);
+            currMaxTime = max(currMaxTime, neededTime[i]);
+        }
+        return totalTime;
+    }
+};
