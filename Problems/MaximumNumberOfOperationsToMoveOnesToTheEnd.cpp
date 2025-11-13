bfb9576bf064b3d39a17da67fe373e3c5d5c03cc
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-13 19:04:50 +0530

    Add solution: MaximumNumberOfOperationsToMoveOnesToTheEnd

---
+++ Problems/MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1828748371 lang=cpp
+ *
+ * MaximumNumberOfOperationsToMoveOnesToTheEnd
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    static int maxOperations(string& s) {
+        int cnt=0, n=s.size(), cnt1=(s[0]=='1');
+        for(int i=1; i<n; i++){
+            cnt1+=s[i]=='1';
+            cnt+=(-(s[i]=='0' &&s[i-1]=='1'))&cnt1;
+        }
+        return cnt;
+    }
+};
