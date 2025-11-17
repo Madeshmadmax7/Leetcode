0a7ff07b7d11ddd4e0ef457f8c791cc41cc66df7
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-17 18:30:51 +0530

    Add solution: CheckIfAll1sAreAtLeastLengthKPlacesAway

---
+++ Problems/CheckIfAll1sAreAtLeastLengthKPlacesAway.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1832298614 lang=cpp
+ *
+ * CheckIfAll1sAreAtLeastLengthKPlacesAway
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        int pre_index;
+        bool f = true;
+
+        for (int i = 0; i < n; i++) {
+            if (f && nums[i] == 1) {
+                f = false;
+                pre_index = i;
+                continue;
+            }
+            if (nums[i] == 1) {
+                if (i - pre_index - 1 >= k)
+                    pre_index = i;
+                else
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
