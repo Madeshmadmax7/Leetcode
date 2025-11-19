887ff3fc9c3d928bb911a0dd761505cf30c33db0
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-19 18:57:28 +0530

    Add solution: AddBinary

---
+++ Problems/AddBinary.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=1834243441 lang=cpp
+ *
+ * AddBinary
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+ public:
+  string addBinary(string a, string b) {
+    string ans;
+    int carry = 0;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    while (i >= 0 || j >= 0 || carry) {
+      if (i >= 0)
+        carry += a[i--] - '0';
+      if (j >= 0)
+        carry += b[j--] - '0';
+      ans += carry % 2 + '0';
+      carry /= 2;
+    }
+
+    reverse(begin(ans), end(ans));
+    return ans;
+  }
+};
