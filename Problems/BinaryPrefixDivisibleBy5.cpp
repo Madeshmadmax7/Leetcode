2c1242baf2f0b15b1e027c1985e0e60140066081
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-24 18:07:35 +0530

    Add solution: BinaryPrefixDivisibleBy5

---
+++ Problems/BinaryPrefixDivisibleBy5.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1838491026 lang=cpp
+ *
+ * BinaryPrefixDivisibleBy5
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        int val = 0;
+        vector<bool> res;
+
+        for (auto& n : nums) {
+            val = ((val << 1) + n) % 5;
+            res.push_back(val == 0);
+        }
+
+        return res;
+    }
+};
