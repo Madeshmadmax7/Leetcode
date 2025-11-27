e1305825517c1f891a321aff9e8684db1047771e
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-11-27 18:09:33 +0530

    Add solution: MaximumSubarraySumWithLengthDivisibleByK

---
+++ Problems/MaximumSubarraySumWithLengthDivisibleByK.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1841005300 lang=cpp
+ *
+ * MaximumSubarraySumWithLengthDivisibleByK
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long> minPrefix(k, LLONG_MAX);
+
+        long long prefix = 0;
+        long long answer = LLONG_MIN;
+
+        minPrefix[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+            int mod = (i + 1) % k;
+
+            if (minPrefix[mod] != LLONG_MAX) {
+                answer = max(answer, prefix - minPrefix[mod]);
+            }
+
+            minPrefix[mod] = min(minPrefix[mod], prefix);
+        }
+
+        return answer;
+    }
+};
