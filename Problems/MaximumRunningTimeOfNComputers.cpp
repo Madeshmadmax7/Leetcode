ab18b0e75f7b35f67526c65c1a61a1a84351c96c
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-12-01 21:15:54 +0530

    Add solution: MaximumRunningTimeOfNComputers

---
+++ Problems/MaximumRunningTimeOfNComputers.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1844253473 lang=cpp
+ *
+ * MaximumRunningTimeOfNComputers
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        long long total = accumulate(arr.begin(), arr.end(), 0LL);
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            if (arr[i] <= total / n) break;
+            total -= arr[i];
+            n--;
+        }
+
+        return total / n;
+    }
+};
