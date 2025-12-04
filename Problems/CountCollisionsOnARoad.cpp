7907926f437ddb034421f9f0c32e951b80c8eb93
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2025-12-04 13:53:03 +0530

    Add solution: CountCollisionsOnARoad

---
+++ Problems/CountCollisionsOnARoad.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1846607262 lang=cpp
+ *
+ * CountCollisionsOnARoad
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    static int countCollisions(string& D) {
+        int n=D.size();
+        if (n==1) return 0;
+        int l=0, r=n-1;
+        while (D[l]=='L') l++;
+        while (l<r && D[r]=='R') r--;
+        if (l>=r) return 0;
+        return count_if(D.begin()+l, D.begin()+(r+1), 
+        [](char c){ return c!='S';});      
+    }
+};
