f279c0c447a0e33d890ed5c55d806324b1f8b43c
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-01-01 20:22:37 +0530

    Add solution: PlusOne

---
+++ Problems/PlusOne.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1871099699 lang=cpp
+ *
+ * PlusOne
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+      int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;  
+    }
+};
