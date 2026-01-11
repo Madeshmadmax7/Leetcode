61e135764f03beb644778baccfc809305cfa344e
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-01-11 20:25:48 +0530

    Add solution: FloodFill

--- Problems/FloodFill.cpp
+++ Problems/FloodFill.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1867726843 lang=cpp
+ * @lc app=leetcode id=1881575548 lang=cpp
  *
  * FloodFill
  * 
- * Difficulty: Medium
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -13,22 +13,22 @@ class Solution {
 public:
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    void dfs(vector<vector<int>>& copy,vector<vector<int>>& image, int row, int col, int color,int initial){
-        copy[row][col]=color;
-        int n=image.size();
-        int m=image[0].size();
+    void bfs(vector<vector<int>>&copy,vector<vector<int>>&img,int sr,int sc,int init,int newc){
+        copy[sr][sc]=newc;
+        int n=img.size();
+        int m=img[0].size();
         for(int i=0;i<4;i++){
-            int nr=dr[i]+row;
-            int nc=dc[i]+col;
-            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==initial && copy[nr][nc]!=color){
-                dfs(copy,image,nr,nc,color,initial);
+            int nr=dr[i]+sr;
+            int nc=dc[i]+sc;
+            if(nr>=0&&nr<n&&nc>=0&&nc<m&&img[nr][nc]==init&&copy[nr][nc]!=newc){
+                bfs(copy,img,nr,nc,init,newc);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>> copy=image;
-        int initial=image[sr][sc];
-        dfs(copy,image,sr,sc,color,initial);
+        vector<vector<int>>copy=image;
+        int init=image[sr][sc];
+        bfs(copy,image,sr,sc,init,color);
         return copy;
     }
 };
