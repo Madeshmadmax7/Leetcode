72fd7cfcf0a06a6b35df62c3754ce215d309deed
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-02-13 21:04:25 +0530

    Add solution: TwoSum

--- Problems/TwoSum.cpp
+++ Problems/TwoSum.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1792342912 lang=cpp
+ * @lc app=leetcode id=1918039801 lang=cpp
  *
  * TwoSum
  * 
- * Difficulty: Easy
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -12,16 +12,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int x1,x2;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target){
-                    x1=i;
-                    x2=j;
-                    break;
+                    return {i,j};
                 }
             }
         }
-        return {x1,x2};
+        return {};
     }
 };
