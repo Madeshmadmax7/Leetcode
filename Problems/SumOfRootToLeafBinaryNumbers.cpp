590324436e3fa67235c92d258eef8d4c3e0333b6
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-02-24 20:53:34 +0530

    Add solution: SumOfRootToLeafBinaryNumbers

---
+++ Problems/SumOfRootToLeafBinaryNumbers.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1929787975 lang=cpp
+ *
+ * SumOfRootToLeafBinaryNumbers
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+private:
+    int dfs(TreeNode* node, int curr) {
+        if (!node) return 0;
+        curr = (curr << 1) | node->val;
+        if (!node->left && !node->right) return curr;
+        return dfs(node->left, curr) + dfs(node->right, curr);
+    }
+};
