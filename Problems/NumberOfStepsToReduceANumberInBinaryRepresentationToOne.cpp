482f1bbbc2e5c13e1360ba155915cd9e04b18dbb
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-02-26 23:13:45 +0530

    Add solution: NumberOfStepsToReduceANumberInBinaryRepresentationToOne

---
+++ Problems/NumberOfStepsToReduceANumberInBinaryRepresentationToOne.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1931795050 lang=cpp
+ *
+ * NumberOfStepsToReduceANumberInBinaryRepresentationToOne
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int l = s.length() - 1;
+        int carry = 0;
+        int count = 0;
+        
+        while (l > 0) {
+            if (s[l] - '0' + carry == 0) {
+                carry = 0;
+                count++;
+            } else if (s[l] - '0' + carry == 2) {
+                carry = 1;
+                count++;
+            } else {
+                carry = 1;
+                count += 2;
+            }
+            l--;
+        }
+        if (carry == 1) {
+            count++;
+        }
+        
+        return count;
+    }
+};
