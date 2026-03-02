ab2cdda0506b7de02ff568ef4d88d9bd31a00ad4
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-02 21:18:56 +0530

    Add solution: BestTimeToBuyAndSellStock

--- Problems/BestTimeToBuyAndSellStock.cpp
+++ Problems/BestTimeToBuyAndSellStock.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1793274778 lang=cpp
+ * @lc app=leetcode id=1935729503 lang=cpp
  *
  * BestTimeToBuyAndSellStock
  * 
- * Difficulty: Easy
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -12,13 +12,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min_price=INT_MAX;
-        int max_profit=0;
+        int minPrice=INT_MAX;
+        int maxProfit=0;
         for(int num:prices){
-            min_price=min(num,min_price);
-            int profit=num-min_price;
-            max_profit=max(profit,max_profit);
+            minPrice=min(minPrice,num);
+            maxProfit=max(maxProfit,num-minPrice);
         }
-        return max_profit;
+        return maxProfit;
     }
 };
