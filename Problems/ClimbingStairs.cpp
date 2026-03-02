31bcca21974240acdb09dd55acef994f14c50202
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-02 21:18:52 +0530

    Add solution: ClimbingStairs

--- Problems/ClimbingStairs.cpp
+++ Problems/ClimbingStairs.cpp
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode id=1934444541 lang=cpp
+ * @lc app=leetcode id=1935690271 lang=cpp
  *
  * ClimbingStairs
  * 
@@ -12,9 +12,9 @@
 class Solution {
 public:
     int solve(vector<int>&dp,int n){
-        if(n==0||n==1) return 1;
+        if(n==1||n==0) return 1;
         if(dp[n]!=-1) return dp[n];
-        return dp[n]=solve(dp,n-1)+solve(dp,n-2);
+        return dp[n] = solve(dp,n-1)+solve(dp,n-2);
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
