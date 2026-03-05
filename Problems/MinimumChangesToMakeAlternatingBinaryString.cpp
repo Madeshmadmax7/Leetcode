f1a07381b6fd37e5fefc477482a0616d526c5a7b
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-05 07:29:04 +0530

    Add solution: MinimumChangesToMakeAlternatingBinaryString

---
+++ Problems/MinimumChangesToMakeAlternatingBinaryString.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1938228637 lang=cpp
+ *
+ * MinimumChangesToMakeAlternatingBinaryString
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int minOperations(string s) {
+        int n = s.length();
+        int cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0 && s[i] != '0') cnt1++;   
+            if(i % 2 != 0 && s[i] != '1') cnt1++;
+        }
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0 && s[i] != '1') cnt2++;   
+            if(i % 2 != 0 && s[i] != '0') cnt2++;
+        }
+        return min(cnt1, cnt2);
+    }
+};
