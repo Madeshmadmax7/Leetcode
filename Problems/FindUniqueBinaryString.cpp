e84bdf006216e704686d97cf22d8729e9665a62a
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-08 18:18:53 +0530

    Add solution: FindUniqueBinaryString

---
+++ Problems/FindUniqueBinaryString.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=1941947624 lang=cpp
+ *
+ * FindUniqueBinaryString
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        int size = pow(2, n);
+
+        vector<int> nu(size, 0);
+
+        for(string num : nums){
+            int val = stoi(num, nullptr, 2);
+            nu[val]++;
+        }
+
+        for(int i = 0; i < size; i++){
+            if(nu[i] == 0){
+                string ans = bitset<32>(i).to_string();
+                ans = ans.substr(32 - n);
+                return ans;
+            }
+        }
+
+        return string(n, '0');
+    }
+};
