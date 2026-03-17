885e70563e92908efecfa7dcdc971b48e4321949
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-17 22:46:40 +0530

    Add solution: NumberOfProvinces

--- Problems/NumberOfProvinces.cpp
+++ Problems/NumberOfProvinces.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1847749676 lang=cpp
+ * @lc app=leetcode id=1951172164 lang=cpp
  *
  * NumberOfProvinces
  * 
- * Difficulty: Medium
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,34 +11,24 @@
 
 class Solution {
 public:
-    void bfs(vector<int>& vis,vector<int> adjL[],int n){
-        vis[n]=1;
-        for(auto it: adjL[n]){
-            if(!vis[it]){
-                bfs(vis,adjL,it);
+    void dfs(int st,vector<vector<int>>&adj,vector<int>&vis,int n){
+        vis[st]=1;
+        for(int i=0;i<n;i++){
+            if(adj[st][i]==1&&!vis[i]){
+                dfs(i,adj,vis,n);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected[0].size();
-        int m = isConnected.size();
-        vector<int> adjList[n];
-        vector<int> vis(n,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isConnected[i][j]==1&&i!=j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
-            }
-        }
-        int c=0;
+    int findCircleNum(vector<vector<int>>& adj) {
+        int n=adj.size();
+        vector<int>vis(n,0);
+        int ct=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                c++;
-                bfs(vis,adjList,i);
+                ct++;
+                dfs(i,adj,vis,n);
             }
         }
-        return c;
+        return ct;
     }
 };
