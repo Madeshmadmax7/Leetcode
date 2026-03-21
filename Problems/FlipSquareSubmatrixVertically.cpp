62bde77bee9fe84697bfb4aae1b294739e3f9af5
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-21 09:19:10 +0530

    Add solution: FlipSquareSubmatrixVertically

---
+++ Problems/FlipSquareSubmatrixVertically.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1954410080 lang=cpp
+ *
+ * FlipSquareSubmatrixVertically
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
+        for(int i=0;i<k/2;i++){
+            for(int j=0;j<k;j++){
+                swap(grid[x+i][y+j],grid[x+k-i-1][y+j]);
+            }
+        }
+        return grid;
+    }
+};
