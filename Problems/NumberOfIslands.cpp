11ca4fe0511637e5ca9434afd19d1d2403d70e74
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-25 17:22:05 +0530

    Add solution: NumberOfIslands

--- Problems/NumberOfIslands.cpp
+++ Problems/NumberOfIslands.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1848085412 lang=cpp
+ * @lc app=leetcode id=1958793018 lang=cpp
  *
  * NumberOfIslands
  * 
- * Difficulty: Level
+ * Difficulty: Medium
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,46 +11,35 @@
 
 class Solution {
 public:
-    void countIslands(vector<vector<int>> &vis, vector<vector<char>>&grid, int r, int c){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        queue<pair<int,int>> que;
-        vis[r][c] = 1;
-        que.push({r,c});
-
-        int dr[4] = {-1, 1, 0, 0};
-        int dc[4] = {0, 0, -1, 1};
-
+    int dr[4]={-1,0,1,0};
+    int dc[4]={0,1,0,-1};
+    void bfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int i,int j,int n,int m){
+        vis[i][j]=1;
+        queue<pair<int,int>>que;
+        que.push({i,j});
         while(!que.empty()){
-            int fr = que.front().first;
-            int fc = que.front().second;
+            int fr=que.front().first;
+            int fc=que.front().second;
             que.pop();
-
-            for(int k = 0; k < 4; k++){
-                int nr = fr + dr[k];
-                int nc = fc + dc[k];
-
-                if(nr>=0 && nr<n && nc>=0 && nc<m &&
-                   !vis[nr][nc] && grid[nr][nc]=='1'){
-                    vis[nr][nc] = 1;
+            for(int k=0;k<4;k++){
+                int nr=dr[k]+fr;
+                int nc=dc[k]+fc;
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]=='1'&&!vis[nr][nc]){
                     que.push({nr,nc});
+                    vis[nr][nc]=1;
                 }
             }
         }
     }
-
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int c = 0;
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    countIslands(vis, grid, i, j);
+        int n=grid.size();
+        int m=grid[0].size();
+        int c=0;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'&&!vis[i][j]){
+                    bfs(grid,vis,i,j,n,m);
                     c++;
                 }
             }
