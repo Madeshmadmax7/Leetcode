b3cab7d256255d98d611748e7fb4ac6cf1f0da28
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-03-25 17:22:02 +0530

    Add solution: EditDistance

---
+++ Problems/EditDistance.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1958716917 lang=cpp
+ *
+ * EditDistance
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.length();
+        int m=word2.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
