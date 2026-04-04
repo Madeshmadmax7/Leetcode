59f40ce24f82dbe89d34edc5f575c52c3f30c1cf
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-04 10:40:21 +0530

    Add solution: DecodeTheSlantedCiphertext

---
+++ Problems/DecodeTheSlantedCiphertext.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1968266257 lang=cpp
+ *
+ * DecodeTheSlantedCiphertext
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int n = encodedText.size();
+        if (rows == 1)
+            return encodedText;
+
+        int cols = n / rows;
+        string res;
+        res.reserve(n);
+
+        for (int c = 0; c < cols; ++c) {
+            int r = 0, j = c;
+            while (r < rows && j < cols) {
+                res += encodedText[r * cols + j];
+                ++r;
+                ++j;
+            }
+        }
+
+        while (!res.empty() && res.back() == ' ') {
+            res.pop_back();
+        }
+
+        return res;
+    }
+};
