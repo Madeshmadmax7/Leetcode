fbb12f1e44f3cc690c251c74746f45ae329e0de3
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-05 21:42:52 +0530

    Add solution: RobotReturnToOrigin

---
+++ Problems/RobotReturnToOrigin.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1969785509 lang=cpp
+ *
+ * RobotReturnToOrigin
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (char m : moves) {
+            if (m == 'R') x++;
+            else if (m == 'L') x--;
+            else if (m == 'U') y++;
+            else if (m == 'D') y--;
+        }
+        return x == 0 && y == 0;
+    }
+};
