b6b351e9ba54cf15844c3c6e5d55638ba0050979
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-08 17:26:44 +0530

    Add solution: XorAfterRangeMultiplicationQueriesI

---
+++ Problems/XorAfterRangeMultiplicationQueriesI.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1972528156 lang=cpp
+ *
+ * XorAfterRangeMultiplicationQueriesI
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& n, vector<vector<int>>& q) {
+        int mod = 1e9 + 7;
+
+        for (auto &a : q) {
+            for (int i = a[0]; i <= a[1]; i += a[2]) {
+                long long p = 1LL * n[i] * a[3];
+                n[i] = (int)(p % mod);
+            }
+        }
+
+        int s = 0;
+        for (int x : n) s ^= x;
+
+        return s;
+    }
+};
