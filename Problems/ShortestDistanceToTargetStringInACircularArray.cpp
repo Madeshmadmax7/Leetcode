09d35ad345c98313ad71cc122ceb767cb425e7f6
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-15 17:31:23 +0530

    Add solution: ShortestDistanceToTargetStringInACircularArray

---
+++ Problems/ShortestDistanceToTargetStringInACircularArray.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1979140489 lang=cpp
+ *
+ * ShortestDistanceToTargetStringInACircularArray
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    static int closestTarget(vector<string>& words, string& target, int startIndex) {
+        const int n=words.size();
+        int d=0;
+        for(; d<=n-1; d++){
+            const int l=startIndex>=d?startIndex-d:n+startIndex-d;
+            const int r=startIndex+d>=n?startIndex+d-n:startIndex+d;
+            const string& x=words[l];
+            const string& y=words[r];
+            if (x==target || y==target) return d;
+        }
+        return -1;
+        
+    }
+};
