912f0af57fd119378dd4c1971e45633c9c28e03d
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-17 17:26:45 +0530

    Add solution: MinimumAbsoluteDistanceBetweenMirrorPairs

---
+++ Problems/MinimumAbsoluteDistanceBetweenMirrorPairs.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1980892868 lang=cpp
+ *
+ * MinimumAbsoluteDistanceBetweenMirrorPairs
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int minMirrorPairDistance(vector<int>& nums) {
+        int res = 100000, i = 0;
+        unordered_map<int, int> seen;
+
+        for (int& n : nums) {
+            int r;
+            if (seen.count(n))
+                res = min(res, i - seen[n]);
+
+            for (r = 0; n; n /= 10)
+                r = r * 10 + (n % 10);
+
+            seen[r] = i++;
+        }
+
+        return -(res == 100000) | res;
+    }
+};
