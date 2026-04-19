1bf1b773dc6f18736522e639b91d270db66ebb56
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-04-19 15:52:18 +0530

    Add solution: MaximumDistanceBetweenAPairOfValues

---
+++ Problems/MaximumDistanceBetweenAPairOfValues.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1982562978 lang=cpp
+ *
+ * MaximumDistanceBetweenAPairOfValues
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    int maxDistance(vector<int>& A, vector<int>& B) {
+        int i, j;
+
+        for (i = 0, j = 0; i < A.size() && j < B.size(); j++)
+            i += A[i] > B[j];
+
+        return max(0, j - i - 1);
+    }
+};
