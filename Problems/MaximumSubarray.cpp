8d754a032abc7750221ae4df159a37cff1bda09f
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-02 13:40:28 +0530

    Add solution: MaximumSubarray

--- Problems/MaximumSubarray.cpp
+++ Problems/MaximumSubarray.cpp
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode id=1935695512 lang=cpp
+ * @lc app=leetcode id=1991031601 lang=cpp
  *
  * MaximumSubarray
  * 
@@ -14,12 +14,16 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxi=INT_MIN;
         int sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum=sum+nums[i];
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
             maxi=max(maxi,sum);
             if(sum<0){
                 sum=0;
             }
+        }
+        if(sum==0){
+            return maxi;
         }
         return maxi;
     }
