a25ffb6c079f46f8e7c9c72f9b0bc4dac5f17d43
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-04 21:19:02 +0530

    Add solution: NumberOfIslands

--- Problems/NumberOfIslands.cpp
+++ Problems/NumberOfIslands.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1958793018 lang=cpp
+ * @lc app=leetcode id=1994972773 lang=cpp
  *
  * NumberOfIslands
  * 
- * Difficulty: Medium
+ * Difficulty: Level
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,10 +11,11 @@
 
 class Solution {
 public:
-    int dr[4]={-1,0,1,0};
-    int dc[4]={0,1,0,-1};
-    void bfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int i,int j,int n,int m){
-        vis[i][j]=1;
+    void bfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int i,int j){
+        int dr[4]={0,1,0,-1};
+        int dc[4]={-1,0,1,0};
+        int n=grid.size();
+        int m=grid[0].size();
         queue<pair<int,int>>que;
         que.push({i,j});
         while(!que.empty()){
@@ -22,11 +23,11 @@ public:
             int fc=que.front().second;
             que.pop();
             for(int k=0;k<4;k++){
-                int nr=dr[k]+fr;
-                int nc=dc[k]+fc;
-                if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]=='1'&&!vis[nr][nc]){
-                    que.push({nr,nc});
+                int nr=fr+dr[k];
+                int nc=fc+dc[k];
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&!vis[nr][nc]&&grid[nr][nc]=='1'){
                     vis[nr][nc]=1;
+                    que.push({nr,nc});
                 }
             }
         }
@@ -34,16 +35,16 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int c=0;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int ct=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1'&&!vis[i][j]){
-                    bfs(grid,vis,i,j,n,m);
-                    c++;
+                    bfs(grid,vis,i,j);
+                    ct++;
                 }
             }
         }
-        return c;
+        return ct;
     }
 };
