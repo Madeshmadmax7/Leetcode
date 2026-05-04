1727dca31ff6e4a500bdcc663e0760b52275a015
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-04 21:19:04 +0530

    Add solution: FloodFill

--- Problems/FloodFill.cpp
+++ Problems/FloodFill.cpp
@@ -1,9 +1,9 @@
 /*
- * @lc app=leetcode id=1881575548 lang=cpp
+ * @lc app=leetcode id=1994996958 lang=cpp
  *
  * FloodFill
  * 
- * Difficulty: Level
+ * Difficulty: Medium
  * Category: undefined
  * Runtime: N/A
  * Memory: N/A
@@ -11,24 +11,29 @@
 
 class Solution {
 public:
-    int dr[4]={-1,0,1,0};
-    int dc[4]={0,1,0,-1};
-    void bfs(vector<vector<int>>&copy,vector<vector<int>>&img,int sr,int sc,int init,int newc){
-        copy[sr][sc]=newc;
-        int n=img.size();
-        int m=img[0].size();
-        for(int i=0;i<4;i++){
-            int nr=dr[i]+sr;
-            int nc=dc[i]+sc;
-            if(nr>=0&&nr<n&&nc>=0&&nc<m&&img[nr][nc]==init&&copy[nr][nc]!=newc){
-                bfs(copy,img,nr,nc,init,newc);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int dr[4]={0,1,0,-1};
+        int dc[4]={-1,0,1,0};
+        int ic=image[sr][sc];
+        if(ic==color) return image;
+        int n=image.size();
+        int m=image[0].size();
+        queue<pair<int,int>>que;
+        que.push({sr,sc});
+        image[sr][sc]=color;
+        while(!que.empty()){
+            int fr=que.front().first;
+            int fc=que.front().second;
+            que.pop();
+            for(int i=0;i<4;i++){
+                int nr=fr+dr[i];
+                int nc=fc+dc[i];
+                if(nr>=0&&nr<n&&nc>=0&&nc<m&&image[nr][nc]==ic){
+                    image[nr][nc]=color;
+                    que.push({nr,nc});
+                }
             }
         }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>>copy=image;
-        int init=image[sr][sc];
-        bfs(copy,image,sr,sc,init,color);
-        return copy;
+        return image;
     }
 };
