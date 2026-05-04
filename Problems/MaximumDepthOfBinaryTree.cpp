af7f0372699892aa59fbb51524c74577c7611e26
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-04 10:40:29 +0530

    Add solution: MaximumDepthOfBinaryTree

---
+++ Problems/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1994628829 lang=cpp
+ *
+ * MaximumDepthOfBinaryTree
+ * 
+ * Difficulty: Level
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+        int l=maxDepth(root->left);
+        int r=maxDepth(root->right);
+        return 1+max(l,r);
+    }
+};
