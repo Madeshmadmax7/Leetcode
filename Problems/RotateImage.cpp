90730852814b0a9052abf7707235cfcec4c84dde
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-04 08:43:03 +0530

    Add solution: RotateImage

---
+++ Problems/RotateImage.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1994611913 lang=cpp
+ *
+ * RotateImage
+ * 
+ * Difficulty: Easy
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(auto &row:matrix){
+            reverse(row.begin(),row.end());
+        }
+    }
+};
