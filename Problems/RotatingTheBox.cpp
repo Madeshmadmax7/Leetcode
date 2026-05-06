9f6b8dbb5a800c086d5ac9875c33a50f123131ba
Madesh A <149663509+Madeshmadmax7@users.noreply.github.com>
2026-05-06 19:50:56 +0530

    Add solution: RotatingTheBox

---
+++ Problems/RotatingTheBox.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=1996654372 lang=cpp
+ *
+ * RotatingTheBox
+ * 
+ * Difficulty: Medium
+ * Category: undefined
+ * Runtime: N/A
+ * Memory: N/A
+ */
+
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+
+        int rows = box.size();
+        int cols = box[0].size();
+
+        for (int r = 0; r < rows; r++) {
+
+            int empty = cols - 1;
+
+            for (int c = cols - 1; c >= 0; c--) {
+
+                if (box[r][c] == '*') {
+                    empty = c - 1;
+                }
+
+                else if (box[r][c] == '#') {
+
+                    swap(box[r][c], box[r][empty]);
+                    empty--;
+                }
+            }
+        }
+
+        vector<vector<char>> ans(cols, vector<char>(rows));
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                ans[c][rows - 1 - r] = box[r][c];
+            }
+        }
+
+        return ans;
+    }
+};
